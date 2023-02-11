cb1d8c2992dbf1c52d4c4d8de868c641ebf27ac4
a-korkin <a-korkin88@yandex.ru>
2023-02-11 16:48:19 +0300

    feature: reindex indexes

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,10 @@ async fn main() {
         .map(|t| t.name.to_owned())
         .collect::<Vec<String>>();
 
-    vacuum_tables(&pool, &tables).await.expect("Failed to VACUUM");
+    vacuum_tables(&pool, &tables).await.expect("Failed to vacuum");
+
+    let indexes = get_indexes(&pool).await.expect("Failed to get indexes");
+    reindex_indexes(&pool, &indexes).await.expect("Failed to reindex");
 }
 
 #[derive(Debug, sqlx::FromRow)]
@@ -22,12 +25,15 @@ pub struct Table {
     pub name: String,
 }
 
+#[derive(Debug, sqlx::FromRow)]
+pub struct Index(String);
+
 async fn get_tables(pool: &PgPool) -> Result<Vec<Table>, sqlx::Error> {
     let tables = sqlx::query_as::<_, Table>(
         r#"
-        SELECT (s.table_schema || '.' || s.table_name) AS name
+        SELECT ('"' || s.table_schema || '"."' || s.table_name || '"') AS name
         FROM information_schema.tables AS s
-        WHERE s.table_schema NOT IN ('information_schema', 'pg_catalog', 'fias', 'sys')        
+        WHERE s.table_schema NOT IN ('information_schema', 'pg_catalog', 'sys')        
             AND s.table_type = 'BASE TABLE'
         ORDER BY s.table_schema, s.table_name;
         "#)
@@ -42,6 +48,29 @@ async fn vacuum_tables(pool: &PgPool, tables: &Vec<String>) -> Result<(), sqlx::
         println!("VACUUM table {}", table);
 
         let _ = sqlx::query(&format!("VACUUM ANALYZE {};", table))
+            .execute(pool)
+            .await;
+    }
+
+    Ok(())
+}
+
+async fn get_indexes(pool: &PgPool) -> Result<Vec<Index>, sqlx::Error> {
+    sqlx::query_as::<_, Index>(
+        r#"
+        SELECT '"' || schemaname || '."' || indexname || '"'
+        FROM pg_indexes
+        WHERE schemaname NOT IN ('information_schema', 'pg_catalog', 'sys')
+        "#)
+        .fetch_all(pool)
+        .await
+}
+
+async fn reindex_indexes(pool: &PgPool, indexes: &Vec<Index>) -> Result<(), sqlx::Error> {
+    for idx in indexes {
+        println!("REINDEX INDEX {}", idx.0);
+
+        let _ = sqlx::query(&format!("REINDEX INDEX {}", idx.0))
             .execute(pool)
             .await;
     }

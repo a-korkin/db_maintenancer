628b027b9682e995ec8818f27e035591f275db48
a-korkin <a-korkin88@yandex.ru>
2023-02-11 16:33:39 +0300

    feature: vacuum tables

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{postgres::PgPoolOptions, types::Uuid};
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
 #[tokio::main]
 async fn main() {
@@ -7,21 +7,44 @@ async fn main() {
         .await
         .expect("Failed to connect to Postgres");
 
-    let persons = sqlx::query_as::<_, Person>(
-        "SELECT id, last_name, first_name FROM public.person;"
-    )
-    .fetch_all(&pool)
-    .await
-    .expect("Failed to execute query");
+    let tables = get_tables(&pool)
+        .await
+        .expect("Failed to get list of tables")
+        .iter()
+        .map(|t| t.name.to_owned())
+        .collect::<Vec<String>>();
 
-    for person in persons {
-        println!("{:#?}", person);
-    }
+    vacuum_tables(&pool, &tables).await.expect("Failed to VACUUM");
 }
 
 #[derive(Debug, sqlx::FromRow)]
-pub struct Person {
-    pub id: Uuid,
-    pub last_name: String,
-    pub first_name: String,
+pub struct Table {
+    pub name: String,
+}
+
+async fn get_tables(pool: &PgPool) -> Result<Vec<Table>, sqlx::Error> {
+    let tables = sqlx::query_as::<_, Table>(
+        r#"
+        SELECT (s.table_schema || '.' || s.table_name) AS name
+        FROM information_schema.tables AS s
+        WHERE s.table_schema NOT IN ('information_schema', 'pg_catalog', 'fias', 'sys')        
+            AND s.table_type = 'BASE TABLE'
+        ORDER BY s.table_schema, s.table_name;
+        "#)
+        .fetch_all(pool)
+        .await;
+
+    tables
+}
+
+async fn vacuum_tables(pool: &PgPool, tables: &Vec<String>) -> Result<(), sqlx::Error> {
+    for table in tables {
+        println!("VACUUM table {}", table);
+
+        let _ = sqlx::query(&format!("VACUUM ANALYZE {};", table))
+            .execute(pool)
+            .await;
+    }
+
+    Ok(())
 }

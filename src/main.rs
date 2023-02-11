6d6c21e6b225110a963a5648e6e2229b4a32571c
a-korkin <a-korkin88@yandex.ru>
2023-02-11 17:06:22 +0300

    feature: config added

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
+use config::{Config, File, FileFormat};
+use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, PgPool};
 
 #[tokio::main]
 async fn main() {
+    let builder = Config::builder()
+        .add_source(File::new("config.yml", FileFormat::Yaml))
+        .build()
+        .expect("Failed to read config")
+        .try_deserialize::<Settings>()
+        .expect("Failed to deserialize config");
+
     let pool = PgPoolOptions::new()
-        .connect("postgres://postgres:password@localhost:5432/test")
+        .connect(&builder.database)
         .await
         .expect("Failed to connect to Postgres");
 
     let tables = get_tables(&pool)
         .await
-        .expect("Failed to get list of tables")
-        .iter()
-        .map(|t| t.name.to_owned())
-        .collect::<Vec<String>>();
-
+        .expect("Failed to get list of tables");
     vacuum_tables(&pool, &tables).await.expect("Failed to vacuum");
 
     let indexes = get_indexes(&pool).await.expect("Failed to get indexes");
     reindex_indexes(&pool, &indexes).await.expect("Failed to reindex");
 }
 
-#[derive(Debug, sqlx::FromRow)]
-pub struct Table {
-    pub name: String,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Settings {
+    pub database: String,
 }
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct Table(String);
 
 #[derive(Debug, sqlx::FromRow)]
 pub struct Index(String);
@@ -31,7 +39,7 @@ pub struct Index(String);
 async fn get_tables(pool: &PgPool) -> Result<Vec<Table>, sqlx::Error> {
     let tables = sqlx::query_as::<_, Table>(
         r#"
-        SELECT ('"' || s.table_schema || '"."' || s.table_name || '"') AS name
+        SELECT ('"' || s.table_schema || '"."' || s.table_name || '"')
         FROM information_schema.tables AS s
         WHERE s.table_schema NOT IN ('information_schema', 'pg_catalog', 'sys')        
             AND s.table_type = 'BASE TABLE'
@@ -43,11 +51,11 @@ async fn get_tables(pool: &PgPool) -> Result<Vec<Table>, sqlx::Error> {
     tables
 }
 
-async fn vacuum_tables(pool: &PgPool, tables: &Vec<String>) -> Result<(), sqlx::Error> {
+async fn vacuum_tables(pool: &PgPool, tables: &Vec<Table>) -> Result<(), sqlx::Error> {
     for table in tables {
-        println!("VACUUM table {}", table);
+        println!("VACUUM table {}", table.0);
 
-        let _ = sqlx::query(&format!("VACUUM ANALYZE {};", table))
+        let _ = sqlx::query(&format!("VACUUM ANALYZE {};", table.0))
             .execute(pool)
             .await;
     }

cdb421889843100d66d3909261eb60ca8c6bd3ef
a-korkin <a-korkin88@yandex.ru>
2023-02-12 09:53:34 +0300

    fix: config builder

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,33 @@
 use config::{Config, FileFormat, File};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Default)]
 pub struct Settings {
     pub database: String,
-    pub excluded_schemas: Vec<String>,
+    excluded_schema_list: Vec<String>,
+    pub excluded_schemas: String,
     pub log_dir: String,
+    pub vacuum: bool,
+    pub reindex: bool,
+    pub refresh_matviews: bool,
 }
 
-pub fn get_config() -> Settings {
-    Config::builder()
-        .add_source(File::new("config.yml", FileFormat::Yaml))
-        .build()
-        .expect("Failed to read config")
-        .try_deserialize::<Settings>()
-        .expect("Failed to deserialize settings")
+impl Settings {
+    pub fn builder() -> Self {
+        let mut settings = Config::builder()
+            .set_default("excluded_schemas", "").expect("Failed to set default")
+            .add_source(File::new("config.yml", FileFormat::Yaml))
+            .build()
+            .expect("Failed to read config")
+            .try_deserialize::<Settings>()
+            .expect("Failed to deserialize settings");
+        
+        settings.excluded_schemas = settings.excluded_schema_list
+            .iter()
+            .map(|s| format!("'{s}'"))
+            .collect::<Vec<String>>()
+            .join(",");
+
+        settings
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
 mod configuration;
 mod db;
 
-use configuration::get_config;
+use configuration::Settings;
 use db::{vacuum_tables, reindex_indexes, refresh_matviews};
 use sqlx::postgres::PgPoolOptions;
 use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
-    let settings = get_config();
-    let file_appender = tracing_appender::rolling::daily(settings.log_dir, "info");
+    let settings = Settings::builder();
+    let file_appender = tracing_appender::rolling::daily(&settings.log_dir, "info");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     tracing_subscriber::fmt()
         .with_writer(non_blocking)
         .init();
-
-    let excluded_schemas = settings.excluded_schemas
-        .iter()
-        .map(|s| format!("'{s}'"))
-        .collect::<Vec<String>>()
-        .join(",");
 
     let pool = PgPoolOptions::new()
         .connect(&settings.database)
         .await
         .expect("Failed to connect to Postgres");
 
-    vacuum_tables(&pool, &excluded_schemas).await.expect("Failed to VACUUM tables");
-    reindex_indexes(&pool, &excluded_schemas).await.expect("Failed to REINDEX indexes");
-    refresh_matviews(&pool).await.expect("Failed to REFRESH matviews");
+    if settings.vacuum {
+        vacuum_tables(&pool, &settings.excluded_schemas)
+            .await
+            .expect("Failed to VACUUM tables");
+    }
+    if settings.reindex {
+        reindex_indexes(&pool, &settings.excluded_schemas)
+            .await
+            .expect("Failed to REINDEX indexes");
+    }
+    if settings.refresh_matviews {
+        refresh_matviews(&pool)
+            .await
+            .expect("Failed to REFRESH matviews");
+    }
 }

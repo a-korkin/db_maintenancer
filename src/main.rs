61eea446b32345b2b30d93d04ee6cfae14f950b3
a-korkin <a-korkin88@yandex.ru>
2023-02-12 07:19:14 +0300

    feature: refreshing materialized views

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,48 @@
 use config::{Config, File, FileFormat};
 use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, PgPool};
-use log::{info, error, LevelFilter};
+use tracing::error;
+use tracing::instrument;
+use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
-    let _ = simple_logging::log_to_file("info.log", LevelFilter::Info);
-
-    let builder = Config::builder()
+    tracing_subscriber::fmt::init();
+    
+    let settings = Config::builder()
         .add_source(File::new("config.yml", FileFormat::Yaml))
         .build()
         .expect("Failed to read config")
         .try_deserialize::<Settings>()
         .expect("Failed to deserialize config");
 
+    let excluded_schemas = settings.excluded_schemas
+        .iter()
+        .map(|s| format!("'{s}'"))
+        .collect::<Vec<String>>()
+        .join(",");
+
     let pool = PgPoolOptions::new()
-        .connect(&builder.database)
+        .connect(&settings.database)
         .await
         .expect("Failed to connect to Postgres");
 
-    let tables = get_tables(&pool)
+    let tables = get_tables(&pool, &excluded_schemas)
         .await
         .expect("Failed to get list of tables");
     vacuum_tables(&pool, &tables).await.expect("Failed to vacuum");
 
-    let indexes = get_indexes(&pool).await.expect("Failed to get indexes");
+    let indexes = get_indexes(&pool, &excluded_schemas).await.expect("Failed to get indexes");
     reindex_indexes(&pool, &indexes).await.expect("Failed to reindex");
+
+    let matviews = get_matviews(&pool).await.expect("Failed to get matviews");
+    refresh_matviews(&pool, &matviews).await.expect("Failed to refresh matviews");
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Settings {
     pub database: String,
+    pub excluded_schemas: Vec<String>,
 }
 
 #[derive(Debug, sqlx::FromRow)]
@@ -39,15 +51,21 @@ pub struct Table(String);
 #[derive(Debug, sqlx::FromRow)]
 pub struct Index(String);
 
-async fn get_tables(pool: &PgPool) -> Result<Vec<Table>, sqlx::Error> {
-    let tables = sqlx::query_as::<_, Table>(
+#[derive(Debug, sqlx::FromRow)]
+pub struct MatView(String);
+
+#[instrument(skip(pool, excluded_schemas))]
+async fn get_tables(pool: &PgPool, excluded_schemas: &str) -> Result<Vec<Table>, sqlx::Error> {
+    let sql = format!(
         r#"
-        SELECT ('"' || s.table_schema || '"."' || s.table_name || '"')
-        FROM information_schema.tables AS s
-        WHERE s.table_schema NOT IN ('information_schema', 'pg_catalog', 'sys')        
-            AND s.table_type = 'BASE TABLE'
-        ORDER BY s.table_schema, s.table_name;
-        "#)
+        SELECT ('"' || table_schema || '"."' || table_name || '"')
+        FROM information_schema.tables
+        WHERE table_schema NOT IN ({})        
+            AND table_type = 'BASE TABLE'
+        ORDER BY table_schema, table_name;
+        "#, excluded_schemas);
+
+    let tables = sqlx::query_as::<_, Table>(&sql)
         .fetch_all(pool)
         .await;
 
@@ -56,8 +74,6 @@ async fn get_tables(pool: &PgPool) -> Result<Vec<Table>, sqlx::Error> {
 
 async fn vacuum_tables(pool: &PgPool, tables: &Vec<Table>) -> Result<(), sqlx::Error> {
     for table in tables {
-        // info!("VACUUM table {}", table.0);
-
         let _ = sqlx::query(&format!("VACUUM ANALYZE {};", table.0))
             .execute(pool)
             .await
@@ -67,25 +83,51 @@ async fn vacuum_tables(pool: &PgPool, tables: &Vec<Table>) -> Result<(), sqlx::E
     Ok(())
 }
 
-async fn get_indexes(pool: &PgPool) -> Result<Vec<Index>, sqlx::Error> {
-    sqlx::query_as::<_, Index>(
+#[instrument(skip(pool, excluded_schemas))]
+async fn get_indexes(pool: &PgPool, excluded_schemas: &str) -> Result<Vec<Index>, sqlx::Error> {
+    let sql = format!(
         r#"
-        SELECT '"' || schemaname || '."' || indexname || '"'
+        SELECT '"' || schemaname || '"."' || indexname || '"'
         FROM pg_indexes
-        WHERE schemaname NOT IN ('information_schema', 'pg_catalog', 'sys')
-        "#)
+        WHERE schemaname NOT IN ({})
+        ORDER BY schemaname, indexname;
+        "#, excluded_schemas);
+
+    sqlx::query_as::<_, Index>(&sql)
         .fetch_all(pool)
         .await
 }
 
 async fn reindex_indexes(pool: &PgPool, indexes: &Vec<Index>) -> Result<(), sqlx::Error> {
     for idx in indexes {
-        // println!("REINDEX INDEX {}", idx.0);
-
-        let _ = sqlx::query(&format!("REINDEX INDEX {}", idx.0))
+        let _ = sqlx::query(&format!("REINDEX INDEX {};", idx.0))
             .execute(pool)
-            .await;
+            .await
+            .map_err(|e| error!("{e}"));
     }
 
+    Ok(())
+}
+
+#[instrument(skip(pool))]
+async fn get_matviews(pool: &PgPool) -> Result<Vec<MatView>, sqlx::Error> {
+    sqlx::query_as::<_, MatView>(
+        r#"
+        SELECT '"' || schemaname || '"."' || matviewname || '"'
+        FROM pg_matviews
+        ORDER BY schemaname, matviewname;
+        "#)
+        .fetch_all(pool)
+        .await
+}
+
+async fn refresh_matviews(pool: &PgPool, matviews: &Vec<MatView>) -> Result<(), sqlx::Error> {
+    for matview in matviews {
+        let _ = sqlx::query(&format!("REFRESH MATERIALIZED VIEW {};", matview.0))
+            .execute(pool)
+            .await
+            .map_err(|e| error!("{e}"));
+    }
+    
     Ok(())
 }
